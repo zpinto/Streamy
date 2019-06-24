83cbd56ed52d9e98fa4f1588711c98e0c43afa4f
Zachary Pinto <zpinto@uci.edu>
2019-06-23 21:07:24 -0700

    add authentication

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -13,19 +13,42 @@ class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in</div>;
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignInClick} className="ui green google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
 
   render() {
     return <div>{this.renderAuthButton()}</div>;
