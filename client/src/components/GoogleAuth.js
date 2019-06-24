f84ddf712b08d937db0b58708e139cf237beebce
Zachary Pinto <zpinto@uci.edu>
2019-06-23 20:46:21 -0700

    setup routing and skeleton with auth

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "842868967904-sv2ftiv61n3csh1o1mi57ml9r4chqclm.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
