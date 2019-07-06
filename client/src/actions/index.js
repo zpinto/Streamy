05d54e5aed87a25ed81c4601521086b1d3fbcc91
Zachary Pinto <zpinto@uci.edu>
2019-07-05 22:03:01 -1000

    rename files

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,8 @@
 import streams from "../apis/streams";
 import {
   SIGN_IN,
-  SIGN_OUT
+  SIGN_OUT,
+  CREATE_STREAM
 } from "./types";
 
 export const signIn = userId => {
@@ -19,4 +20,9 @@ export const signOut = () => {
 
 export const createStream = formValues => async dispatch => {
   const response = await streams.post("/streams", formValues);
+
+  dispatch({
+    type: CREATE_STREAM,
+    payload: response.data
+  })
 };
