fa4e8dc3dbbd6b74fdbbda718f4e2ffb45bfaea3
Zachary Pinto <zpinto@uci.edu>
2019-07-05 21:48:37 -1000

    create new stream action creater

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,22 @@
+import streams from "../apis/streams";
 import {
-    SIGN_IN,
-    SIGN_OUT
-} from './types'
+  SIGN_IN,
+  SIGN_OUT
+} from "./types";
 
-export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    };
-}
+export const signIn = userId => {
+  return {
+    type: SIGN_IN,
+    payload: userId
+  };
+};
 
 export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    };
-}
+  return {
+    type: SIGN_OUT
+  };
+};
+
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post("/streams", formValues);
+};
